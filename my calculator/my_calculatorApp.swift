d2c59e35f40bfbbe7ee024b5f690ffc43cfa791e
clssra <saracolosio@icloud.com>
2021-10-22 11:28:23 +0200

    Initial Commit

---
+++ my calculator/my_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_calculatorApp.swift
+//  my calculator
+//
+//  Created by clssra on 22/10/21.
+//
+
+import SwiftUI
+
+@main
+struct my_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
